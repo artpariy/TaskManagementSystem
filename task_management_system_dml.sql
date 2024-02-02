e72d5e5837eaeb2d996048829694e21bf28136a6
Artyom <140423753+artpariy@users.noreply.github.com>
2024-02-02 22:10:12 +0300

    Update task_management_system_dml.sql

--- task_management_system_dml.sql
+++ task_management_system_dml.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (email, password)
 VALUES
-('user1@mail.com', '$2a$10$KN//hNIGuMvW0Gcvrnoet.6vL0x6vJA.Gyr/2.IF/X3/1Z0JeKZsi'),
+('user1@mail.com', '$2a$10$KN//hNIGuMvW0Gcvrnoet.6vL0x6vJA.Gyr/2.IF/X3/1Z0JeKZsi'), /*Пароль для всех пользователей 123*/
 ('user2@mail.com', '$2a$10$Yyc0bl8FbXv.OSriBt0Wi.7zN3hpn3uBvjRcAD1D6vnynxi0fVZui'),
 ('user3@mail.com', '$2a$10$7WCEPZ8irSC7SoYAtQiOFOhj7rJwVfjsWUaYSKMD49UJufwGSUb5q'),
 ('user4@mail.com', '$2a$10$qtSFxH/JOOpi2rnL5nC60u03sfNLsro6cU/i34gti68ErAFpaHTD.');
